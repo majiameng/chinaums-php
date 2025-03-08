0a77066000849a2737710001ab2c5bef94d42e63
Tinymeng <666@majiameng.com>
2025-03-08 13:58:05 +0800

    commit add

---
+++ src/Interfaces/ProviderInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tinymeng\Chinaums\Interfaces;
+
+interface ProviderInterface
+{
+    public function pay($order);
+
+    public function find($order);
+
+    public function cancel($order);
+
+    public function close($order);
+
+    public function refund($order);
+
+    public function callback($contents);
+
+    public function success();
+}
---
+++ src/Provider/Wechat.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace tinymeng\Chinaums\Provider;
+
+use tinymeng\Chinaums\Interfaces\ProviderInterface;
+use tinymeng\Chinaums\Tools\Verify;
+use tinymeng\Chinaums\Tools\Str;
+
+use Exception;
+
+class Wechat implements ProviderInterface
+{
+    protected $config = [];
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public function __call(string $shortcut, array $params)
+    {
+        $class = '\\tinymeng\\Chinaums\\Service\\Wechat\\' . Str::studly($shortcut);
+        $objcet = new $class();
+        $objcet->setConfig($this->config);
+        return $objcet;
+    }
+
+    public function pay($order)
+    {
+        return $this->__call('Mini', [])->request($order);
+    }
+
+    public function find($order)
+    {
+        return $this->__call('Query', [])->request($order);
+    }
+
+    public function cancel($order)
+    {
+        throw new Exception("Wechat does not support cancel api");
+    }
+
+    public function close($order)
+    {
+        return $this->__call('Close', [])->request($order);
+    }
+
+    public function refund($order)
+    {
+        return $this->__call('Refund', [])->request($order);
+    }
+
+    public function callback($contents)
+    {
+        $params = array_map(function ($value) {
+            return urldecode($value);
+        }, $contents);
+        $md5Key = $this->config['md5key'];
+        $sign = Verify::makeSign($md5Key, $params);
+        $notifySign = array_key_exists('sign', $params) ? $params['sign'] : '';
+        if (strcmp($sign, $notifySign) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public function success()
+    {
+        return 'SUCCESS';
+    }
+}
