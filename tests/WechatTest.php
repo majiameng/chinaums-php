7850c64e5affec0fbe6a8ae4371d9c35db80a73e
Tinymeng <666@majiameng.com>
2025-03-09 15:38:32 +0800

    添加支付宝支付和云闪付支付

--- tests/WechatTest.php
+++ tests/WechatTest.php
@@ -10,8 +10,6 @@ class WechatTest extends TestCase
 {
     public function testAddress()
     {
-
-        var_dump(1111111);
         $data = [];
         // 报文请求时间
         $data['requestTimestamp'] = date("YmdHis", time());
@@ -30,18 +28,11 @@ class WechatTest extends TestCase
         $data['tradeType'] = 'JSAPI';
 
         $config = [
-            // 请求网关  https://api-mop.chinaums.com/v1
-            'gateway' => 'https://test-api-open.chinaums.com/v1',
-            // 商户号
-            'mid' => '8982****5678',
-            // 终端号
-            'tid' => '88****01',
-            // 加密 APPID
-            'appid' => '10037e6****5e5a0006',
-            // 加密 KEY
-            'appkey' => '1c4e3b1606****6a09e5b312e8',
-            // 回调验证需要的md5key
-            'md5key' => 'impA***********hCaTCXJ6'
+            'mid' => '8982****5678',// 商户号
+            'tid' => '88****01',// 终端号
+            'appid' => '10037e6****5e5a0006',// 加密 APPID
+            'appkey' => '1c4e3b1606****6a09e5b312e8',// 加密 KEY
+            'md5key' => 'impA***********hCaTCXJ6'// 回调验证需要的md5key
         ];
         Factory::config($config);
         $app = Factory::Wechat()->mini();
